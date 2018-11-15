4b5b52de9c84a1e15a4a59f6bb41d4fa75324881
yeonyang9421 <chris09+>
2018-11-15 22:46:10 +0900

    서비스, 인텐트 서비스 -> 서비스 시작 후 종료버튼이 눌리기 전에는 서비스가 종료되지 않으며, 자신이 작업이 끝난 후에는 자동으로 destroy되어 자원을 돌려준다. foreground service(알림으로 앱이 실행중임을 알려주는 서비스), 바인드 서비스(앱이 보이지 않아도 알림에서 작업을 계속 액티비티와 연결되어 실행 할 수 있는 서비스 예로 음악 플레이어는 앱이 보이지 않아도 알림으로 음악이 재생 중임을 알림으로 받아서 실행을 계속 할 수 있다.

--- app/src/main/java/kr/co/woobi/imyeon/self_study_service/MainActivity.java
+++ app/src/main/java/kr/co/woobi/imyeon/self_study_service/MainActivity.java
@@ -1,12 +1,19 @@
 package kr.co.woobi.imyeon.self_study_service;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-        private  int mCount;
+    private MyService myService;
+    private  boolean mBound;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +34,53 @@ public class MainActivity extends AppCompatActivity {
     public void onStatIntentService(View view) {
         Intent intent=new Intent(this, MyService.class);
         startService(intent);
+    }
+
+    public void onStatForegroundService(View view) {
+        Intent intent=new Intent(this, MyService.class);
+        intent.setAction("startForeground");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent);
+            }else{
+            startService(intent);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent=new Intent(this, MyService.class);
+        bindService(intent, mConnection, BIND_AUTO_CREATE);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mBound){
+            unbindService(mConnection);
+            mBound=false;
+        }
+    }
+
+    private ServiceConnection mConnection  = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyService.MyBinder binder = (MyService.MyBinder)service;
+            myService = binder.getService();
+            mBound=true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            //예기치못한 상황에서 종료되었을떄 호출되는 함수
+
+        }
+    };
+
+    public void getCountValue(View view) {
+        if(mBound) {
+            Toast.makeText(this, "카운팅 : " + myService.getCount(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/kr/co/woobi/imyeon/self_study_service/MyService.java
+++ app/src/main/java/kr/co/woobi/imyeon/self_study_service/MyService.java
@@ -1,27 +1,46 @@
 package kr.co.woobi.imyeon.self_study_service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class MyService extends Service {
     private static final String TAG = MyService.class.getSimpleName();
     private Thread mThread;
-    int mCount;
+    int mCount = 0;
+
+    private IBinder mBinder = new MyBinder();
+
+    public class MyBinder extends Binder {
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+
     public MyService() {
     }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(mThread==null){
-            mThread=new Thread("My Thread"){
+        if ("startForeground".equals(intent.getAction())) {
+            startForegroundService();
+        } else if (mThread == null) {
+            mThread = new Thread("My Thread") {
                 @Override
                 public void run() {
-                    for(int i=0; i<5; i++){
-                        try{
-                        mCount++;
-                        Thread.sleep(1000);
-                    }catch(InterruptedException e){
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            mCount++;
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
                             break;
                         }
                         Log.d("My Service", "서비스 동작 중" + mCount);
@@ -34,19 +53,40 @@ public class MyService extends Service {
         return START_NOT_STICKY;
     }
 
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy : ");
-        if(mThread !=null){
+        if (mThread != null) {
             mThread.interrupt();
-            mThread=null;
-            mCount=0;
+            mThread = null;
+            mCount = 0;
         }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-       throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+    public int getCount() {
+        return mCount;
+    }
+
+    private void startForegroundService() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("포그리운드 서비스");
+        builder.setContentText("포그라운드 서비스 실행중");
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        builder.setContentIntent(pendingIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(new NotificationChannel("default", "기본채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+        startForeground(1, builder.build());
     }
 }
